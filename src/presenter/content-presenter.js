6232d5492be81bd6361b72bb959a17523f5147a2
Сергей Савельев <carrion19870317@gamil.com>
2022-08-13 16:41:25 +0300

    Фикс

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,8 +1,8 @@
-import ListFilterView from './view/list-filter-view.js';
-import ListSortView from './view/list-sort-view.js';
-import UserRatingView from './view/user-rating-view.js';
-import ShowMoreButtonView from './view/showmore-button-view.js';
-import FilmCardView from './view/film-card-view.js';
+import ListFilterView from '../view/list-filter-view.js';
+import ListSortView from '../view/list-sort-view.js';
+import UserRatingView from '../view/user-rating-view.js';
+import ShowMoreButtonView from '../view/showmore-button-view.js';
+import FilmCardView from '../view/film-card-view.js';
 import FilmListView from '../view/film-list-view.js';
 import FilmDetailsView from '../view/film-details-view.js';
 import { render } from '../render.js';
@@ -17,6 +17,7 @@ export default class ContentPresenter {
     render(new UserRatingView(), this.filmListComponent.getElement());
     render(new ListFilterView(), this.filmListComponent.getElement());
     render(new ListSortView(), this.filmListComponent.getElement());
+    render(new FilmDetailsView(), this.filmListComponent.getElement());
 
     for(let i = 0; i < 5; i++) {
       render(new FilmCardView(), this.filmListComponent.getElement());
